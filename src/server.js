cbc803a5d169cd38f1cace2c6635cae5db79ef6d
Dimas Gabriel <dimas@dimasgabriel.net>
2019-08-08 11:46:48 -0300

    Handling errors when starting mongoose and the server

--- src/server.js
+++ src/server.js
@@ -6,12 +6,19 @@ require("dotenv").config();
 
 const server = express();
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true
-});
-
 server.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(process.env.PORT || 3333);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, err => {
+  if (err) {
+    return console.error("Cant connect to mongoose...");
+  }
+
+  const port = process.env.PORT || 3333;
+  server.listen(port, () => {
+    console.log(`Server running on PORT ${port}...`);
+  }).on("error", e => {
+    console.error(`Error starting server: ${e}`);
+  });
+});

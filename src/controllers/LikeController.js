562461d4afe2a64bf908241cc0a44a1d0448019c
Dimas Gabriel <dimas@dimasgabriel.net>
2019-08-08 11:25:37 -0300

    Refactoring to use just a single LikeController for like and dislike actions

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,29 +1,41 @@
 const Dev = require("../models/Dev");
 const mongoose = require("mongoose");
 
-module.exports = {
-  async create(req, res) {
-    const { devId } = req.params;
-    const { user } = req.headers;
+async function likeAction(req, res, isLike) {
+  const { devId } = req.params;
+  const { user } = req.headers;
 
-    if (!mongoose.Types.ObjectId.isValid(devId) || !mongoose.Types.ObjectId.isValid(user)) {
-      return res.status(400).json({ error: "Bad request" });
-    }
+  if (!mongoose.Types.ObjectId.isValid(devId) || !mongoose.Types.ObjectId.isValid(user)) {
+    return res.status(400).json({ error: "Bad request" });
+  }
 
-    const loggedDev = await Dev.findById(user);
-    const targetDev = await Dev.findById(devId);
+  const loggedDev = await Dev.findById(user);
+  const targetDev = await Dev.findById(devId);
 
-    if (!targetDev) {
-      return res.status(404).json({ error: "Target dev not found" });
-    }
+  if (!targetDev) {
+    return res.status(404).json({ error: "Target dev not found" });
+  }
 
+  if (isLike) {
     if (targetDev.likes.includes(user)) {
       console.log("MATCH");
     }
 
     loggedDev.likes.push(targetDev._id);
-    await loggedDev.save();
+  } else {
+    loggedDev.dislikes.push(targetDev._id);
+  }
 
-    return res.json(loggedDev);
+  await loggedDev.save();
+  return res.json(loggedDev);
+}
+
+module.exports = {
+  async like(req, res) {
+    return likeAction(req, res, true);
+  },
+
+  async dislike(req, res) {
+    return likeAction(req, res, false);
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,11 @@ const express = require("express");
 const routes = express.Router();
 const DevController = require("./controllers/DevController");
 const LikeController = require("./controllers/LikeController");
-const DislikeController = require("./controllers/DislikeController");
-
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello fuckers!" });
-});
 
 routes.get("/devs", DevController.list);
 routes.post("/devs", DevController.create);
 
-routes.post("/devs/:devId/likes", LikeController.create);
-routes.post("/devs/:devId/dislikes", DislikeController.create);
+routes.post("/devs/:devId/likes", LikeController.like);
+routes.post("/devs/:devId/dislikes", LikeController.dislike);
 
 module.exports = routes;
